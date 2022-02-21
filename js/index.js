7c5dc10b461540ee8fefbce3e3974253f4ffe63a
kabyl-ulan <kabylulanabdykalykuulu@gmail.com>
2022-02-22 00:16:02 +0600

    initial

---
+++ js/index.js
@@ -0,0 +1,14 @@
+$('.movie').slick({
+    slidesToShow: 4,
+    // dots: true,
+    // centerMode: true,
+    // centerPadding: '40px'
+})
+
+$('.video').slick({
+    slidesToShow: 2,
+})
+
+$('.actor').slick({
+    slidesToShow: 4
+})
