2dbf2dd577a2b93de454700638f6ddd84c9c8b8d
kabyl-ulan <kabylulanabdykalykuulu@gmail.com>
2022-02-24 17:09:37 +0600

    resposive

--- js/index.js
+++ js/index.js
@@ -3,12 +3,77 @@ $('.movie').slick({
     // dots: true,
     // centerMode: true,
     // centerPadding: '40px'
+    responsive:[
+        {
+            breakpoint: 1024,
+            settings:{
+                slidesToShow: 3,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 970,
+            settings:{
+                slidesToShow: 2,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 1,
+                arrows: false
+            }
+        }
+    ]
 })
 
 $('.video').slick({
     slidesToShow: 2,
+
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 2,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 990,
+            settings: {
+                slidesToShow: 1,
+                arrows: false
+            }
+        },
+
+    ]
 })
 
 $('.actor').slick({
-    slidesToShow: 4
+    slidesToShow: 4,
+
+    responsive: [
+        {
+            breakpoint: 1025,
+            settings: {
+                slidesToShow: 3,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 970,
+            settings:{
+                slidesToShow: 2,
+                arrows: false
+            }
+        },
+        {
+            breakpoint: 640,
+            settings: {
+                slidesToShow: 1,
+                arrows: false
+            }
+        }
+    ]
 })
